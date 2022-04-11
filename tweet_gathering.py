15efee205d6daa986386575c8216156c498a9b44
Juan Camilo Sabogal Olarte <sabogalo@msu.edu>
2022-04-11 01:01:41 -0400

    Serialization of the Data Frame obtained through YouTube's API

--- tweet_gathering.py
+++ tweet_gathering.py
@@ -11,6 +11,7 @@ query = None
 
 #Information that we need to extract from each tweet. See the structure of a tweet in the Twitter API Documentation
 attributes = ["id",'author_id', 'created_at',"entities","conversation_id"]
+
 """
 Input: number of tweets required, query, attributes
 Output: list of tweets (tweet: object) with the attributes specified
@@ -29,7 +30,7 @@ Output: dataframe with each tweet as a row, and columns correspond to
         the attributes of the tweet
 """
 def organize_tweets_in_dataframe(tweet_list):
-    dictionary = {}
+    dictionary = {}  # Dictinoary used to construct Data Frame
 
     sample_tweet = tweet_list[0]
     for key in sample_tweet.keys():
@@ -47,8 +48,3 @@ def organize_tweets_in_dataframe(tweet_list):
     df.insert(0, 'id', first_column)
 
     return df
-
-
-
-
-
